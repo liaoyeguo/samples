26eb0db50211a3886d148276b8175b6f5eb668ac
liaoyeguo <liaoyeguo@163.com>
2022-03-22 15:34:55 +0800

    final version

---
+++ express-stream/src/index.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { Readable } from "stream";
+const app = express();
+
+const delayResolve = (res: any, time = 0) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(res), time);
+  });
+};
+
+app.get("/", (req, res, next) => {
+  res.send("hello");
+});
+
+app.get("/stream", async (req, res, next) => {
+  res.status(200);
+  res.type("text/html; charset=utf-8");
+  res.set("Transfer-Encoding", "chunked");
+
+  const resStream = Readable.from([
+    delayResolve("<!DOCTYPE html><html>"),
+    delayResolve("<head><title>Express Stream Demo</title></head>", 0),
+    delayResolve("<body>"),
+    delayResolve("<h1>Express Stream Demo<h1>"),
+    delayResolve("<div>1</div>"),
+    delayResolve("<div>2</div>", 1000),
+    delayResolve("<div>3</div>", 2000),
+    delayResolve("</body></html>"),
+  ]);
+
+  resStream.pipe(res);
+});
+
+app.listen(3000);
