d4fdf1e3af25ff808ef6f2d2e28e4622f67e73f7
liaoyeguo <liaoyeguo@163.com>
2022-03-22 15:36:39 +0800

    add comment

--- express-stream/src/index.ts
+++ express-stream/src/index.ts
@@ -17,12 +17,13 @@ app.get("/stream", async (req, res, next) => {
   res.type("text/html; charset=utf-8");
   res.set("Transfer-Encoding", "chunked");
 
+  // 数组里的Promise是并发的，通过依次增加延时来模拟串行的效果
   const resStream = Readable.from([
     delayResolve("<!DOCTYPE html><html>"),
     delayResolve("<head><title>Express Stream Demo</title></head>", 0),
     delayResolve("<body>"),
     delayResolve("<h1>Express Stream Demo<h1>"),
-    delayResolve("<div>1</div>"),
+    delayResolve("<div>1</div>", 500),
     delayResolve("<div>2</div>", 1000),
     delayResolve("<div>3</div>", 2000),
     delayResolve("</body></html>"),
